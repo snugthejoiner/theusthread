812187d77f8fa55c5689b36089fca655efb6ad03
snugthejoiner <joiner@jedparish.com>
2015-08-03 21:51:39 -0400

    remove birthday from users model

--- db/migrate/20150802180743_devise_create_users.rb
+++ db/migrate/20150802180743_devise_create_users.rb
@@ -5,7 +5,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :username,           null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.date :birthdate
 
       ## Recoverable
       t.string   :reset_password_token
