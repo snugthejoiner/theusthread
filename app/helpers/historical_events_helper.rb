18b669864aa899bf147032e3d653bdbcd143fd5b
snugthejoiner <joiner@jedparish.com>
2015-08-14 21:30:35 -0400

    created a helper named blurb to display hist data

--- app/helpers/historical_events_helper.rb
+++ app/helpers/historical_events_helper.rb
@@ -1,2 +1,13 @@
 module HistoricalEventsHelper
+
+    def blurb(historical_event)
+      if historical_event.premilestone_status.to_s != ''
+        "\n #{historical_event.premilestone_status}"
+      elsif historical_event.ongoing_status.to_s != ''
+        "\n #{historical_event.ongoing_status}"
+      else
+        ""
+      end
+    end
+
 end
