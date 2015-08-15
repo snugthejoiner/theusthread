f58962883e247af3b526d8d36a6c11983e4f7381
snugthejoiner <joiner@jedparish.com>
2015-08-15 11:09:05 -0400

    first attempt at a historical_events_helper spec

--- spec/helpers/historical_events_helper_spec.rb
+++ spec/helpers/historical_events_helper_spec.rb
@@ -10,6 +10,12 @@ require 'rails_helper'
 #     end
 #   end
 # end
+
 RSpec.describe HistoricalEventsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "blurb" do
+    it "returns a premilestone_status or ongoing_status if present or an empty string if neither are present" do
+      historical_event = HistoricalEvent.new(premilestone_status: 'the wheel has not been invented')
+        expect(historical_event.blurb).to match /the wheel has not been invented/
+      end
+    end
 end
