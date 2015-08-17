1553ffc08c1a4b6809daf2faa880a78a337e88d7
snugthejoiner <joiner@jedparish.com>
2015-08-16 23:42:17 -0400

    specs for User

--- spec/helpers/historical_events_helper_spec.rb
+++ spec/helpers/historical_events_helper_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe HistoricalEventsHelper, type: :helper do
     it "returns a premilestone_status or ongoing_status if present or an empty string if neither are present" do
       historical_event = HistoricalEvent.new(premilestone_status: 'the wheel has not been invented')
       expect(helper.blurb(historical_event)).to eq("\n the wheel has not been invented")
+      historical_event = HistoricalEvent.new(ongoing_status: 'grass is still green')
+      expect(helper.blurb(historical_event)).to eq("\n grass is still green")
+      historical_event = HistoricalEvent.new
+      expect(helper.blurb(historical_event)).to eq("")
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe User
-    it "is invalid without a username" do
-      FactoryGirl.build(:user, username: nil).should_not be_valid
+    it "is invalid without an email" do
+      FactoryGirl.build(:user, email: nil).should_not be_valid
+    end
+    it "is invalid without a password" do
+      FactoryGirl.build(:user, password: nil).should_not be_valid
     end
 end
