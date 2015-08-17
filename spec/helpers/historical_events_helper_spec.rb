8fe6b30a24dd302439dc089a8384f891489c2c3d
snugthejoiner <joiner@jedparish.com>
2015-08-16 23:53:59 -0400

    updated hist_events_helper_spec with Faker data

--- spec/helpers/historical_events_helper_spec.rb
+++ spec/helpers/historical_events_helper_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe HistoricalEventsHelper, type: :helper do
   describe "blurb" do
+    status = Faker::Lorem.sentence
     it "returns a premilestone_status or ongoing_status if present or an empty string if neither are present" do
-      historical_event = HistoricalEvent.new(premilestone_status: 'the wheel has not been invented')
-      expect(helper.blurb(historical_event)).to eq("\n the wheel has not been invented")
-      historical_event = HistoricalEvent.new(ongoing_status: 'grass is still green')
-      expect(helper.blurb(historical_event)).to eq("\n grass is still green")
+      historical_event = HistoricalEvent.new(premilestone_status: status)
+      expect(helper.blurb(historical_event)).to eq("\n #{status}")
+      historical_event = HistoricalEvent.new(ongoing_status: status)
+      expect(helper.blurb(historical_event)).to eq("\n #{status}")
       historical_event = HistoricalEvent.new
       expect(helper.blurb(historical_event)).to eq("")
     end
