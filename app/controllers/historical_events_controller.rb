bf359afe7d8000b918310eae836db8c57beb481d
snugthejoiner <joiner@jedparish.com>
2015-09-02 14:26:51 -0400

    Personal and Historical events are in hist_events/index

--- app/controllers/historical_events_controller.rb
+++ app/controllers/historical_events_controller.rb
@@ -3,7 +3,7 @@ class HistoricalEventsController < ApplicationController
   # http://stackoverflow.com/questions/3310947/how-do-i-mix-2-rails-models-into-a-single-find
 
   def index
-    @events = (HistoricalEvent.all + PersonalEvent.all)
+    @events = (HistoricalEvent.all + PersonalEvent.user_people)
   end
   
 end
--- app/models/personal_event.rb
+++ app/models/personal_event.rb
@@ -2,8 +2,8 @@ class PersonalEvent < ActiveRecord::Base
   belongs_to :person
   has_many :tags, as: :tagable
 
-  def event_start
-    self.starting.strftime('%m/%d/%Y')
-  end
+  current_user = User.last
+
+  scope :user_people, -> { where(person_id: current_user.people)}
 
 end
