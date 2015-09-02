5f02f64ec4616b4f8a65586c19ec78d67fa2d67a
snugthejoiner <joiner@jedparish.com>
2015-09-02 13:00:18 -0400

    more work combining hist_events and personal_events for hist_events/index

--- app/models/personal_event.rb
+++ app/models/personal_event.rb
@@ -2,8 +2,8 @@ class PersonalEvent < ActiveRecord::Base
   belongs_to :person
   has_many :tags, as: :tagable
 
-  def pe_uid
-    self.person.user.id
+  def event_start
+    self.starting.strftime('%m/%d/%Y')
   end
 
 end
