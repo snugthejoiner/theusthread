96928a75f464a44dc756bc824b4a5faf2c2a4d87
Brittany Martin <brittany.jill.martin@gmail.com>
2015-09-21 22:03:05 -0400

    Set up production mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain => "theusthread.com",
+    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+    :password => ENV['MAILGUN_SMTP_PASSWORD'],
+  }
+
+  config.action_mailer.default_url_options = { host: 'theusthread.com' }
+  config.action_mailer.raise_delivery_errors = true
 end
