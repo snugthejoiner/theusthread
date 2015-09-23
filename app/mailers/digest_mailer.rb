2d42e261976a9eee0cf789669a806c3d4f66e937
snugthejoiner <joiner@jedparish.com>
2015-09-23 13:11:15 -0400

    building monthly digest mailer

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -1,9 +1,16 @@
 class DigestMailer < ApplicationMailer
 
-  def digest_email(user, person)
+  default from:"no-reply@theusthread.com"
+
+  def digest_email(user)
+    @historical_events = HistoricalEvent.monthy
     @user = user
-    @person = person
-    mail(to: user.email, subject: "New comment on #{person.name}")
+    @user.people.each do |p|
+      binding.pry
+      pes = PersonalEvent.where(person_id: p.id).personal_monthy
+      @personal_event << pes
+    end
+    mail(to: user.email, subject: "This month's events")
   end
 
 end
